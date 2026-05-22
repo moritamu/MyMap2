e7565f914cf4241da0794480453a392f9912b641
morisum <moris@r.sannet.ne.jp>
2026-05-22 09:27:58 +0900

    Initial Commit

---
+++ MyMap2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyMap2
+//
+//  Created by MsMacM on 2026/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyMap2/MyMap2App.swift
@@ -0,0 +1,17 @@
+//
+//  MyMap2App.swift
+//  MyMap2
+//
+//  Created by MsMacM on 2026/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MyMap2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
